9e7ba2ba7d5e753674c0648b44339c200ebabbbe
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-12 09:52:07 +0900

    예제 2,3,4 실습

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,7 @@ namespace csloop
     {
         static void Main(string[] args)
         {
+            /*
             long start = DateTime.Now.Ticks; //내부적으로 흐른 시간
             long count = 0;
             while (start + (10000000) > DateTime.Now.Ticks)
@@ -32,6 +35,39 @@ namespace csloop
                 Console.WriteLine(item);
             }
             Console.WriteLine();
+
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+            }
+            */
+            /*
+            //예제 02
+            int[] intarray = new int[10];
+            foreach(var item in intarray)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            /*
+            //예제 3
+            int[] array = new int[20];
+            int[] array2 = new int[3] { 1, 2, 3 };
+            int[] array3 = int[] { 1,2,3};
+            int[] array4 = { 1, 2, 3 };
+            */
+            /*
+            //예제 4
+            for(int i = 0; i < 20; i = i + 2)
+            {
+                Console.WriteLine("출력");
+            }
+            */
+
+
+
 
 
 
