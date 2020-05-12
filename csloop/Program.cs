7e7385771f53d8ce2eca93cfcf85505385d97792
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-12 17:03:57 +0900

    개선된 switch 조건문 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -92,7 +92,7 @@ namespace csloop
                 x++;
             }
             */
-
+            /*
             while (true)
             {
                 ConsoleKeyInfo info = Console.ReadKey();
@@ -113,8 +113,30 @@ namespace csloop
                         break;
                 }
             }
-
-
+            */
+            bool state = true;
+            while (state)
+            {
+                ConsoleKeyInfo info = Console.ReadKey();
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("위로 이동");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("오른쪽으로 이동");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("아래로 이동");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("왼쪽으로 이동");
+                        break;
+                    case ConsoleKey.X:
+                        state = false;
+                        break;
+                }
+            }
 
         }
     }
