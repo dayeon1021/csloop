b739677f99d95a6d9410053a66a5e50ea5e3901c
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-12 09:54:45 +0900

    예제 5번 실습

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -65,7 +65,13 @@ namespace csloop
                 Console.WriteLine("출력");
             }
             */
-
+            //예제 5
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("출력");
+                i++;
+            }
 
 
 
